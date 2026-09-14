79bb76e6a491e8ebfd6db2a4987469d7c2cda9be
manish91082-coder <manish91082@gmail.com>
2026-09-15 00:49:05 +0530

    fix: correct native USDC checksum

--- tests/evm/PolygonForkSmoke.t.sol
+++ tests/evm/PolygonForkSmoke.t.sol
@@ -31,7 +31,7 @@ contract PolygonForkSmokeTest {
     address internal constant UNISWAP_V3_ROUTER = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
     address internal constant UNISWAP_V3_QUOTER_V1 = 0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6;
     address internal constant UNISWAP_V3_FACTORY = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
-    address internal constant NATIVE_USDC = 0x3c499c542cef5e3811e1192ce70d8cc03d5c3359;
+    address internal constant NATIVE_USDC = 0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359;
     address internal constant WMATIC = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
     address internal constant WETH = 0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619;
 
