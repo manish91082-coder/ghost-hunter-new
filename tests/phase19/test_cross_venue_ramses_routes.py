f6cdc542b86ab6a2148a2cc14fbbc36c9e118282
manish91082-coder <manish91082@gmail.com>
2026-09-19 01:47:49 +0530

    fix(P0): align cross-venue route test doubles with adapter APIs

--- tests/phase19/test_cross_venue_ramses_routes.py
+++ tests/phase19/test_cross_venue_ramses_routes.py
@@ -40,7 +40,7 @@ def make_quote(token_in, token_out, amount_in, amount_out, dex, fee):
     return QuoteSnapshot(**payload)
 
 
-class FakeQuoter:
+class FakeV3Quoter:
     def __init__(self, token_in, token_out, output, dex, fee):
         self.token_in = token_in
         self.token_out = token_out
@@ -54,10 +54,30 @@ class FakeQuoter:
         return make_quote(token_in, token_out, amount_in, self.output, self.dex, self.fee)
 
 
+class FakeV2Quoter:
+    def __init__(self, token_in, token_out, output, dex):
+        self.token_in = token_in
+        self.token_out = token_out
+        self.output = output
+        self.dex = dex
+
+    def quote_snapshot(self, amount_in, path, *args, **kwargs):
+        if tuple(path) != (self.token_in, self.token_out):
+            raise AssertionError("unexpected QuickSwap V2 path")
+        return make_quote(
+            self.token_in,
+            self.token_out,
+            amount_in,
+            self.output,
+            self.dex,
+            0,
+        )
+
+
 class CrossVenueRamsesRouteTests(unittest.TestCase):
     def test_qsv2_to_ramses_v3_route_preserves_exact_leg_continuity(self):
-        qs = FakeQuoter(A, B, 101_000, "quickswap_v2", 0)
-        rv3 = FakeQuoter(B, A, 100_700, "ramses_v3:pool", 13)
+        qs = FakeV2Quoter(A, B, 101_000, "quickswap_v2")
+        rv3 = FakeV3Quoter(B, A, 100_700, "ramses_v3:pool", 13)
         sim = build_quickswap_v2_to_ramses_v3_route(
             None, qs, rv3, amount_in=100_000, token_a=A, token_b=B,
             ramses_tick_spacing=1, block=BLOCK,
@@ -68,8 +88,8 @@ class CrossVenueRamsesRouteTests(unittest.TestCase):
         self.assertEqual(sim.legs[1].amount_in, 101_000)
 
     def test_ramses_v3_to_qsv2_route_preserves_exact_leg_continuity(self):
-        rv3 = FakeQuoter(A, B, 101_000, "ramses_v3:pool", 13)
-        qs = FakeQuoter(B, A, 100_700, "quickswap_v2", 0)
+        rv3 = FakeV3Quoter(A, B, 101_000, "ramses_v3:pool", 13)
+        qs = FakeV2Quoter(B, A, 100_700, "quickswap_v2")
         sim = build_ramses_v3_to_quickswap_v2_route(
             None, qs, rv3, amount_in=100_000, token_a=A, token_b=B,
             ramses_tick_spacing=1, block=BLOCK,
@@ -78,7 +98,7 @@ class CrossVenueRamsesRouteTests(unittest.TestCase):
         self.assertEqual(sim.legs[1].amount_in, 101_000)
 
     def test_qsv3_to_ramses_v3_route_preserves_exact_leg_continuity(self):
-        qs = FakeQuoter(A, B, 101_100, "quickswap_v3:pool", 37)
+        qs = FakeV3Quoter(A, B, 101_100, "quickswap_v3:pool", 37)
         rv3 = FakeQuoter(B, A, 100_900, "ramses_v3:pool", 13)
         sim = build_quickswap_v3_to_ramses_v3_route(
             None, qs, rv3, amount_in=100_000, token_a=A, token_b=B,
@@ -89,8 +109,8 @@ class CrossVenueRamsesRouteTests(unittest.TestCase):
         self.assertEqual(sim.legs[1].fee_raw, 13)
 
     def test_ramses_v3_to_qsv3_route_preserves_exact_leg_continuity(self):
-        rv3 = FakeQuoter(A, B, 101_100, "ramses_v3:pool", 13)
-        qs = FakeQuoter(B, A, 100_900, "quickswap_v3:pool", 41)
+        rv3 = FakeV3Quoter(A, B, 101_100, "ramses_v3:pool", 13)
+        qs = FakeV3Quoter(B, A, 100_900, "quickswap_v3:pool", 41)
         sim = build_ramses_v3_to_quickswap_v3_route(
             None, qs, rv3, amount_in=100_000, token_a=A, token_b=B,
             ramses_tick_spacing=1, block=BLOCK,
