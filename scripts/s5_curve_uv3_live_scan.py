5654b59e2588d47ddea581ef74553bca8b7937fe
manish91082-coder <manish91082@gmail.com>
2026-09-21 17:55:50 +0530

    fix: bind S5 opportunity discovery import

--- scripts/s5_curve_uv3_live_scan.py
+++ scripts/s5_curve_uv3_live_scan.py
@@ -27,6 +27,7 @@ from phantomx.market_block import acquire_market_block
 from phantomx.uniswap_v3 import UniswapV3ExactQuoter
 from phantomx.rpc_failover import build_free_polygon_rpc_pool
 from phantomx.dynamic_pair_surface import discover_live_base_pairs
+from phantomx.opportunity_discovery import OpportunityDiscoveryResult, discover_exact_opportunities
 try:
     from first_hunt_live_scan import (
         UNISWAP_V3_FACTORY,
